3f98a374d969eb19fe1bcade1cb5ae7d39bd3e8d
Stephan Wentz <swentz@brainbits.net>
2015-05-27 17:01:27 +0200

    fix typo

--- src/Extractor/RawImageExtractor.php
+++ src/Extractor/RawImageExtractor.php
@@ -27,7 +27,7 @@ class RawImageExtractor implements ExtractorInterface
      */
     public function supports($filename, MediaType $mediaType, Specification $targetFormat)
     {
-        return $targetFormat instanceof Image && ($mediaType->getCategory() === 'image' || $mediaType->getType() === 'pdf');
+        return $targetFormat instanceof Image && ($mediaType->getCategory() === 'image' || $mediaType->getName() === 'pdf');
     }
 
     /**
