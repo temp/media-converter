6e8410abdff177e6be9813978878afeaee101857
Stephan Wentz <swentz@brainbits.net>
2016-02-27 18:51:54 +0100

    add mp4box support, fixes

---
+++ tests/Binary/MP4BoxFunctionalTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Temp\MediaConverter\Tests\Binary;
+
+use Temp\MediaConverter\Binary\MP4Box;
+
+class MP4BoxFunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @group functional
+     */
+    public function testProcess()
+    {
+        $mp4box = MP4Box::create();
+        $mp4box->process(__DIR__ . '/../files/Video.mp4');
+    }
+
+    /**
+     * @group functional
+     */
+    public function testProcessWithCustomOutput()
+    {
+        $out = sys_get_temp_dir().'/OutVideo.mp4';
+
+        if (file_exists($out)) {
+            unlink($out);
+        }
+
+        $mp4box = MP4Box::create();
+        $mp4box->process(__DIR__ . '/../files/Video.mp4', $out);
+        $this->assertTrue(file_exists($out));
+        unlink($out);
+    }
+
+    /**
+     * @group functional
+     * @expectedException \Temp\MediaConverter\Binary\Exception\RuntimeException
+     */
+    public function testProcessFail()
+    {
+        $mp4box = MP4Box::create();
+        $mp4box->process(__DIR__ . '/../files/WrongFile.mp4');
+    }
+}
